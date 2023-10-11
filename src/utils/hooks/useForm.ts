794afe5d4daf73ce891fbde5a365f07c02dd4e3b
Ignacio arriagada <iarriagada@zionit.cl>
2023-10-11 10:18:28 -0300

    fix: opciones por defecto en register y correccion comentario

--- src/utils/hooks/useForm.ts
+++ src/utils/hooks/useForm.ts
@@ -3,11 +3,11 @@ import { useForm as useReactHookForm, UseFormProps, RegisterOptions, Path } from
 export function useForm<T extends object>(props: UseFormProps<T>) {
   const { register: innerRegister, ...reactHookForm } = useReactHookForm(props)
 
-  function register(name: Path<T>, options: RegisterOptions | undefined = undefined) {
+  function register(name: Path<T>, options: RegisterOptions | undefined = {}) {
     return {
       ...innerRegister(name, options),
       errorMessage: reactHookForm.formState.errors[name]?.message ?? "",
-      // TODO: esta control de mensajes de error es para next-ui, cambiar dependiente de la implamentacion
+      // TODO: este control de mensajes de error es para next-ui, cambiar dependiendo de la implamentacion
     }
   }
 
