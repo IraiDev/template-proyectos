f52b0a5e4dc345452a49ae9637b4d5e89e544b72
Ignacio arriagada <iarriagada@zionit.cl>
2023-10-13 10:29:20 -0300

    update: implementacion de estado de carga y hook useSearchParams

--- src/utils/hooks/usePagination.ts
+++ src/utils/hooks/usePagination.ts
@@ -1,40 +1,49 @@
 import { useEffect, useState } from "react"
-import { useSearchParams } from "react-router-dom"
+import { useSearchParams } from "."
 import searchParams from "@configs/searchParams"
 
 const { limit, page: pageQuery } = searchParams
 
-export function usePagination({ total }: { total: number }) {
-  const [page, setPage] = useState(Number(pageQuery.default))
-  const [searchParam, setSearchParam] = useSearchParams()
+interface Props {
+  total: number
+  isLoading?: boolean
+}
 
-  const totalPages = Math.ceil((total || 1) / Number(searchParam.get(limit.query) ?? limit.default))
+export function usePagination({ total, isLoading }: Props) {
+  const [page, setPage] = useState(+pageQuery.default)
+  const { searchParams, setSearchParams, getSearchParam } = useSearchParams<"pagina">()
+
+  useEffect(() => {
+    setPage(+getSearchParam("pagina", "1"))
+  }, [getSearchParam])
+
+  const totalPages = Math.ceil((total || 1) / +getSearchParam("pagina", limit.default))
 
   const onChangePage = (value: number) => {
+    if (isLoading) return
+
     setPage(value)
-    searchParam.set(pageQuery.query, value.toString())
-    setSearchParam(searchParam)
+    searchParams.set(pageQuery.query, value.toString())
+    setSearchParams(searchParams)
   }
 
   const onPrevious = () => {
-    if (page === 1) return
+    if (page === 1 || isLoading) return
+
     const newPage = page - 1
     setPage(newPage)
-    searchParam.set(pageQuery.query, newPage.toString())
-    setSearchParam(searchParam)
+    searchParams.set(pageQuery.query, newPage.toString())
+    setSearchParams(searchParams)
   }
 
   const onNext = () => {
-    if (page === totalPages) return
+    if (page === totalPages || isLoading) return
+
     const newPage = page + 1
     setPage(newPage)
-    searchParam.set(pageQuery.query, newPage.toString())
-    setSearchParam(searchParam)
+    searchParams.set(pageQuery.query, newPage.toString())
+    setSearchParams(searchParams)
   }
-
-  useEffect(() => {
-    setPage(Number(searchParam.get(pageQuery.query) ?? 1))
-  }, [searchParam])
 
   return {
     totalPages,
