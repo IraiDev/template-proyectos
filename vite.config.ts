8fdeff060dc2a4caa86177d50589c1cd2131470b
Ignacio arriagada <iarriagada@zionit.cl>
2024-04-10 11:33:14 -0400

    update: implementacion de million para hacer react mas rapido

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite"
+import million from "million/compiler"
 import react from "@vitejs/plugin-react-swc"
 import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [million.vite({ auto: true }), react()],
   build: {
     outDir: "../../front-end/output",
   },
