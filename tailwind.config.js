f06b2674d4c55b47a77e5cfeac2545a2c90addb2
Ignacio arriagada <iarriagada@zionit.cl>
2024-07-25 16:14:49 -0400

    update: integracion de materiasl tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 import { nextui } from "@nextui-org/react"
 import { zinc, red } from "tailwindcss/colors"
 import { PRIMARY_COLORS, SECONDARY_COLORS } from "./src/config/colors"
+import moduleName from "@material-tailwind/react"
 
 /** @type {import('tailwindcss').Config} */
 export default {
